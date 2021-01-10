bcdf22a43eae9cfb17295c55684801190cf43d20
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-01-10 22:51:49 +0000

    header

--- src/common/logger.py
+++ src/common/logger.py
@@ -1,7 +1,7 @@
 '''
 Copyright (C) 2021 Siterummage
 All Rights Reserved.
- 
+
 NOTICE:  All information contained herein is, and remains the property of
 Siterummage.  The intellectual and technical concepts contained herein are
 proprietary to Siterummage and may be covered by U.K. and Foreign Patents,
--- src/common/mysql_connector/mysql_connection.py
+++ src/common/mysql_connector/mysql_connection.py
@@ -1,5 +1,13 @@
 '''
-Copyright (C) 2021 Siterummage Development Team
+Copyright (C) 2021 Siterummage
+All Rights Reserved.
+
+NOTICE:  All information contained herein is, and remains the property of
+Siterummage.  The intellectual and technical concepts contained herein are
+proprietary to Siterummage and may be covered by U.K. and Foreign Patents,
+patents in process, and are protected by trade secret or copyright law.
+Dissemination of this information or reproduction of this material is strictly
+forbidden unless prior written permission is obtained from Siterummage.
 '''
 import mysql.connector
 
