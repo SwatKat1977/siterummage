2697eb588caf81b60264b04acfd5082737a78ae9
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-01-10 22:11:01 +0000

    work on common code

--- src/page_store/__init__.py
+++ src/page_store/__init__.py
@@ -8,6 +8,29 @@ from quart import Quart
 ## Quart application instance
 app = Quart(__name__)
 
+
+@app.before_serving
+async def startup() -> None:
+    """!@brief Code executed before Quart has began serving http requests.
+    @return None
+    """
+    # app.service_task = asyncio.ensure_future(service.start())
+    print('[DEBUG] @app.before_serving')
+
+@app.after_serving
+async def shutdown() -> None:
+    """!@brief Code executed after Quart has stopped serving http requests.
+    @return None
+    """
+    # service.signal_shutdown_requested()
+
+    # while not service.shutdown_completed:
+    #     await asyncio.sleep(0.5)
+    print('[DEBUG] @app.after_serving')
+
+# if not service.initialise():
+#     sys.exit()
+
 mysql_adaptor = MySQLAdaptor('root', 'siterummage', '127.0.0.1', 4000, pool_size=1)
 
 foo = mysql_adaptor.connect('master_2021')
