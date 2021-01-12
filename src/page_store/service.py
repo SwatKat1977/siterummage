221a22679c7925ae4ff1699880819da66f9466ac
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-01-12 09:13:52 +0000

    work in progress

--- src/page_store/service.py
+++ src/page_store/service.py
@@ -9,6 +9,7 @@ patents in process, and are protected by trade secret or copyright law.
 Dissemination of this information or reproduction of this material is strictly
 forbidden unless prior written permission is obtained from Siterummage.
 '''
+from time import sleep
 from common.logger import Logger, LogType
 from common.core_version import CORE_VERSION
 from common.mysql_connector.mysql_adaptor import MySQLAdaptor
@@ -60,6 +61,9 @@ class Service(ServiceBase):
                                         self._db_settings.pool_name,
                                         self._db_settings.pool_size)
 
+        self._api_health = None
+        self._api_links = None
+
     def _initialise(self) -> bool:
         self._logger.write_to_console = True
         self._logger.initialise()
@@ -77,33 +81,44 @@ class Service(ServiceBase):
         return True
 
     def _database_connection_valid(self) -> True:
-        try:
-            test_connection = self._db_adaptor.connect("master_2021")
+        retries = 30
 
-        except RuntimeError as caught_exception:
-            if str(caught_exception) == 'Incorrect user name or password':
-                self._logger.log(LogType.Error,
-                                 'Invalid usernane or password for the ' \
-                                 'database, failed connect to the database')
-                return False
+        self._logger.log(LogType.Info, f'MySQL host is : {self._db_settings.host}')
 
-            if str(caught_exception) == 'Database does not exist':
-                self._logger.log(LogType.Error,
-                                 'Database does not exist, unable to ' \
-                                 'connect to the database')
-                return False
+        while retries != 0:
+            try:
+                self._logger.log(LogType.Info, f'Try : {retries}')
+                test_connection = self._db_adaptor.connect("master_2021")
+                test_connection.close()
+                self._logger.log(LogType.Info, 'Connection to database verified...')
+                return True
 
-            if str(caught_exception) == 'Unable to connect to database server':
-                self._logger.log(LogType.Error,
-                                 'Unable to contact database server...')
-                return False
+            except RuntimeError as caught_exception:
 
-            raise caught_exception
+                if str(caught_exception) == 'Incorrect user name or password':
+                    self._logger.log(LogType.Error,
+                                    'Invalid usernane or password for the ' \
+                                    'database, failed connect to the database')
+                    test_connection.close()
+                    return False
 
-        test_connection.close()
+                if str(caught_exception) == 'Database does not exist':
+                    self._logger.log(LogType.Error,
+                                    'Database does not exist, unable to ' \
+                                    'connect to the database')
+                    test_connection.close()
+                    return False
 
-        self._logger.log(LogType.Info, 'Connection to database verified...')
-        return True
+                if str(caught_exception) == 'Unable to connect to database server':
+                    self._logger.log(LogType.Error,
+                                    'Unable to contact to database, retrying...')
+
+                retries -= 1
+                sleep(10)
+
+                # raise caught_exception
+
+        return False
 
     async def _main_loop(self):
         # if not self._master_thread_class.initialise():
