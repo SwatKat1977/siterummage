11ae4886dba81bba8a9b5d84029c4a126614d5ae
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-01-18 20:40:02 +0000

    schema

--- src/page_store/configuration_schema.py
+++ src/page_store/configuration_schema.py
@@ -9,3 +9,69 @@ patents in process, and are protected by trade secret or copyright law.
 Dissemination of this information or reproduction of this material is strictly
 forbidden unless prior written permission is obtained from Siterummage.
 '''
+
+class configurationSchema:
+    ''' Definition of the configuration files JSON Schema'''
+    #pylint: disable=too-few-public-methods
+
+    class Elements:
+        ''' Definition of the configuration files JSON elements'''
+        #pylint: disable=too-few-public-methods
+
+        # -- Top-level json elements --
+        toplevel_db_settings = 'db settings'
+
+        # -- Database Settings sub-elements --
+        # ------------------------------------
+        db_settings_database = 'database'
+        db_settings_host = 'host'
+        db_settings_pool_name = 'pool_name'
+        db_settings_pool_size = 'pool_size'
+        db_settings_port = 'port'
+        db_settings_username = 'username'
+
+    json_schema = \
+    {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+
+        "type" : "object",
+        "additionalProperties" : False,
+
+        "properties":
+        {
+            'database settings':
+            {
+                "additionalProperties" : False,
+                "properties":
+                {
+                    'database':
+                    {
+                        "type" : "string"
+                    },
+                    'host':
+                    {
+                        "type" : "string"
+                    },
+                    'pool_name':
+                    {
+                        "type" : "string"
+                    },
+                    'pool_size':
+                    {
+                        "type" : "int"
+                    },
+                    'port':
+                    {
+                        "type" : "int"
+                    },
+                    'username':
+                    {
+                        "type" : "string"
+                    }
+                },
+                "required" : ['database', 'host', 'pool_name', 'pool_size',
+                              'port', 'username']
+            }
+        },
+        "required" : ['database settings']
+    }
