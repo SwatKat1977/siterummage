f016a0643471155444687f07b85550082b1a021b
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-01-20 12:21:08 +0000

    removed lint warnings

--- src/common/url_utils.py
+++ src/common/url_utils.py
@@ -12,21 +12,34 @@ forbidden unless prior written permission is obtained from Siterummage.
 from enum import Enum
 
 class UrlType(Enum):
+    """ Enumeration for type of URL (e.g. http or secure http) """
     http = 0
     https = 1
     unknown = 99
 
 class UrlPrefix:
+    """ Prefixes to urls such as http and https """
+    #pylint: disable=too-few-public-methods
+
     http = 'http://'
     https = 'https://'
 
 class UrlUtils:
+    """ General utilities to use with a URL """
+    #pylint: disable=too-few-public-methods
 
     @staticmethod
     def split_url_into_domain_and_page(url):
+        """!@brief Take a full url and break it into a domain, url and type of
+                   url.
+        @param url URL to break into constituant pieces.
+        @returns a dictionary containing the following keys: domain,  url_path,
+                 url_type.  If the url prefix is unknown then domain and
+                 url_path is set to None and url_type is UrlType.unknown
+        """
 
         url_type = UrlType.unknown
-        
+
         index_for_domain = -1
         if url.startswith(UrlPrefix.http):
             index_for_domain = len(UrlPrefix.http)
